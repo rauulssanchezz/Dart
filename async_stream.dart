a1940373553024bf984f9cec82d3ae0e4cd326a9
Raul Sanchez Sanchez <rauulssanchezz@gmail.com>
2025-06-18 15:23:56 +0200

    final commit

---
+++ async_stream.dart
@@ -0,0 +1,13 @@
+void main() {
+  emitNumbers().listen((value) {
+    print('Stream value: $value');
+  });
+}
+
+Stream<int> emitNumbers() async* {
+  final valuesToEmit = [1, 2, 3, 4, 5];
+  for (final value in valuesToEmit) {
+    await Future.delayed(const Duration(seconds: 1));
+    yield value;
+  }
+}
